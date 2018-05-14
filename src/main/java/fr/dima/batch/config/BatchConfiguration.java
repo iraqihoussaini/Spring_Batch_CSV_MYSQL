7db0666378684e300a3804ca6c7e944f9ce3ea86
yassine iraqi <yassineiraqihoussaini@gmail.com>
2018-05-14 17:41:51 +0200

    mise a jour

--- src/main/java/fr/dima/batch/config/BatchConfiguration.java
+++ src/main/java/fr/dima/batch/config/BatchConfiguration.java
@@ -1,9 +1,7 @@
 package fr.dima.batch.config;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
-import java.io.IOException;
 
 import javax.sql.DataSource;
 
@@ -30,24 +28,43 @@ import fr.dima.batch.listener.JobCompletionNotificationListener;
 import fr.dima.batch.model.Personne;
 import fr.dima.batch.processor.PersonItemProcessor;
 
+
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
 
 	@Autowired
 	public JobBuilderFactory jobBuilderFactory;
-
 	@Autowired
 	public StepBuilderFactory stepBuilderFactory;
-
 	@Autowired
 	public DataSource dataSource;
+	@Autowired
+	public ApplicationContext appContext;
+
+
+	
+	
+	@Bean
+	public JdbcBatchItemWriter<Personne> writer() {
+		JdbcBatchItemWriter<Personne> writer = new JdbcBatchItemWriter<Personne>();
+		writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Personne>());
+		writer.setSql("INSERT INTO contact (email,nom, prenom,societe,tel) VALUES (:email, :nom, :prenom, :societe, :tel)");
+		writer.setDataSource(dataSource);
+		return writer;
+		
+		
+	
+	}
+	@Bean
+	public PersonItemProcessor processor() {
+		return new PersonItemProcessor();
+	}
 
 	@Bean
-	public FlatFileItemReader<Personne> reader() throws FileNotFoundException, IOException {
+	public FlatFileItemReader<Personne> reader() {
 		File file = new File(System.getProperty("user.home") + "/ERP_DIMA/");
 		File[] files = file.listFiles(new FilenameFilter() {
-
 			@Override
 			public boolean accept(File dir, String name) {
 				if (name.toLowerCase().endsWith(".csv")) {
@@ -63,16 +80,14 @@ public class BatchConfiguration {
 		System.out.println(files[0].getName());
 		System.out.println("file:" + System.getProperty("user.home") + "/ERP_DIMA/" + files[0].getName());
 		FlatFileItemReader<Personne> reader = new FlatFileItemReader<Personne>();
-		ApplicationContext appContext = new ClassPathXmlApplicationContext(new String[] {});
-		Resource resource = appContext
-				.getResource("file:" + System.getProperty("user.home") + "/ERP_DIMA/" + files[0].getName());
-		// reader.setResource(new ClassPathResource("fichierAIntegrer.csv"));
+		appContext = new ClassPathXmlApplicationContext(new String[] {});
+		Resource resource = appContext.getResource("file:" + System.getProperty("user.home") + "/ERP_DIMA/" + files[0].getName());
 		reader.setResource(resource);
 		reader.setLineMapper(new DefaultLineMapper<Personne>() {
 			{
 				setLineTokenizer(new DelimitedLineTokenizer(";") {
 					{
-						setNames(new String[] { "nom", "prenom", "email", "societe", "tel" });
+						setNames(new String[] {"email" ,"nom", "prenom", "societe", "tel" });
 					}
 				});
 				setFieldSetMapper(new BeanWrapperFieldSetMapper<Personne>() {
@@ -85,31 +100,26 @@ public class BatchConfiguration {
 		return reader;
 	}
 
+
+
+
 	@Bean
-	public PersonItemProcessor processor() {
-		return new PersonItemProcessor();
+	public Job importUserJob(JobCompletionNotificationListener listener) {
+		return jobBuilderFactory.get("importUserJob")
+				.incrementer(new RunIdIncrementer())
+				.listener(listener)
+				.flow(step1())
+				.end()
+				.build();
 	}
 
 	@Bean
-	public JdbcBatchItemWriter<Personne> writer() {
-		JdbcBatchItemWriter<Personne> writer = new JdbcBatchItemWriter<Personne>();
-		writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Personne>());
-		writer.setSql(
-				"INSERT INTO contact (email,nom, prenom,societe,tel) VALUES (:societe,:nom, :prenom,:email,:tel)");
-		writer.setDataSource(dataSource);
-		return writer;
-	}
-
-	@Bean
-	public Job importUserJob(JobCompletionNotificationListener listener) throws FileNotFoundException, IOException {
-		return jobBuilderFactory.get("importUserJob").incrementer(new RunIdIncrementer()).listener(listener)
-				.flow(step1()).end().build();
-	}
-
-	@Bean
-	public Step step1() throws FileNotFoundException, IOException {
-		return stepBuilderFactory.get("step1").<Personne, Personne>chunk(10).reader(reader()).processor(processor())
-				.writer(writer()).build();
+	public Step step1() {
+		return stepBuilderFactory.get("step1").<Personne, Personne>chunk(10)
+				.reader(reader())
+				.processor(processor())
+				.writer(writer())
+				.build();
 	}
 
 }
--- src/main/resources/db_schema.sql
+++ src/main/resources/db_schema.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS contact;
 
 CREATE TABLE contact  (
-    id BIGINT auto_increment NOT NULL PRIMARY KEY,
-    email VARCHAR(100),
+    
+    email VARCHAR(100) PRIMARY KEY ,
     nom VARCHAR(40),
     prenom VARCHAR(40),
     societe VARCHAR(50),
